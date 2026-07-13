da6504f20d42c7baf500d557ed27d2ae771aa9e7
Luis Moya <86807831+byLuisMoya@users.noreply.github.com>
2026-07-13 10:34:04 +0200

    Create nova.rb Cask for Nova app
    Add Cask for Nova application with version 1.0

---
+++ Casks/nova.rb
@@ -0,0 +1,19 @@
+cask "nova" do
+  version "1.0"
+  sha256 "b5e093b18e9b9967edf208d4e6497569c78463d0ff3dc8cfc78f9fad73bbf1c3"
+
+  url "https://github.com/byLuisMoya/Nova/releases/download/v#{version}/Nova-#{version}.zip"
+  name "Nova"
+  desc "Monitor de sensores para la barra de menú en Apple Silicon"
+  homepage "https://github.com/byLuisMoya/Nova"
+
+  depends_on macos: ">= :ventura"
+  depends_on arch: :arm64
+
+  app "Nova.app"
+
+  zap trash: [
+    "~/Library/LaunchAgents/io.github.byluismoya.Nova.plist",
+    "~/Library/Preferences/io.github.byluismoya.Nova.plist",
+  ]
+end
